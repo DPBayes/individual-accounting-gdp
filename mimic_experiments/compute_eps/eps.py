831c13d967cbab69418b4308e1b78f87b98c290d
Solosneros <24623119+Solosneros@users.noreply.github.com>
2023-02-08 11:14:16 +0200

    feat: update the algorithm for converting to (eps, delta)-DP

--- mimic_experiments/compute_eps/eps.py
+++ mimic_experiments/compute_eps/eps.py
@@ -1,5 +1,7 @@
-from scipy import special
 import numpy as np
+from scipy import special
+from scipy.optimize import root_scalar
+import warnings
 
 
 def get_epsilon_using_norm(
@@ -8,18 +10,20 @@ def get_epsilon_using_norm(
     max_clipping_constant: float,
     delta=1e-5,
 ):
-
-    epsilon_grid = np.linspace(0.1, 40, num=int(1e5))[::-1]
-
     def Phi(z):
         return 0.5 * (1 + special.erf(z / np.sqrt(2)))
 
     sigma_ep = noise_multiplier / np.sqrt(
         (squared_clipping_norm / max_clipping_constant ** 2)
     )
-    delta_grid = Phi(1 / (2 * sigma_ep) - epsilon_grid * sigma_ep) - np.exp(
-        epsilon_grid
-    ) * Phi(-1 / (2 * sigma_ep) - epsilon_grid * sigma_ep)
-    best_delta = np.searchsorted(delta_grid, delta, side="right")
 
-    return epsilon_grid[best_delta]
+    def obtain_delta(epsilon, delta, sigma_ep):
+        return Phi(1 / (2 * sigma_ep) - epsilon * sigma_ep) - np.exp(
+            epsilon) * Phi(-1 / (2 * sigma_ep) - epsilon * sigma_ep) - delta
+
+    result_object = root_scalar(obtain_delta, args=(delta, sigma_ep), bracket=[0, 200], method="bisect")
+
+    if not result_object.converged:
+        warnings.warn("The algorithm for retrieving epsilon has not converged.")
+
+    return result_object.root
